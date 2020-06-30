d3846dd51540ffb29a79f133867640fa990b3c20
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-06-30 20:07:28 +0530

    new project

--- src/component/slideShow/Slide.js
+++ src/component/slideShow/Slide.js
@@ -17,7 +17,7 @@ const Slideshow = () => {
           <li className="booking-card b7">
             <div className="book-container">
               <div className="content">
-              <button className="btn"><a href='https://agile-chamber-11888.herokuapp.com/' className="alink">GraphQL-server</a></button>
+              <button className="btn"><a href='https://github.com/ayush-020198/graphql-server' className="alink">GraphQL-server</a></button>
               </div>
             </div>
           </li>
