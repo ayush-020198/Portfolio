d76554393c39d0b45a06616d4cc9b1cecf1cc41a
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-06-30 20:15:04 +0530

    blank-target

--- src/component/slideShow/Slide.js
+++ src/component/slideShow/Slide.js
@@ -9,55 +9,55 @@ const Slideshow = () => {
           <li className="booking-card b1">
             <div className="book-container">
               <div className="content">
-                <button className="btn"><a href='https://agile-chamber-11888.herokuapp.com/' className="alink">Mailsify</a></button>
-                <button className="btn"><a href='https://github.com/ayush-020198/Mailsify' className="alink">Code</a></button>
+                <button className="btn"><a href='https://agile-chamber-11888.herokuapp.com/' target="_blank" rel="noopener noreferrer" className="alink">Mailsify</a></button>
+                <button className="btn"><a href='https://github.com/ayush-020198/Mailsify' target="_blank" rel="noopener noreferrer" className="alink">Code</a></button>
               </div>
             </div>
           </li>
           <li className="booking-card b7">
             <div className="book-container">
               <div className="content">
-              <button className="btn"><a href='https://github.com/ayush-020198/graphql-server' className="alink">GraphQL-server</a></button>
+              <button className="btn"><a href='https://github.com/ayush-020198/graphql-server' target="_blank" rel="noopener noreferrer" className="alink">GraphQL-server</a></button>
               </div>
             </div>
           </li>
           <li className="booking-card b2">
             <div className="book-container">
               <div className="content">
-                <button className="btn"><a href='https://face-webapp.herokuapp.com/' className="alink">Face-Recog-App</a></button>
-                <button className="btn"><a href='https://github.com/ayush-020198/face_recogn_bac' className="alink">Code</a></button>
+                <button className="btn"><a href='https://face-webapp.herokuapp.com/' target="_blank" rel="noopener noreferrer" className="alink">Face-Recog-App</a></button>
+                <button className="btn"><a href='https://github.com/ayush-020198/face_recogn_bac' target="_blank" rel="noopener noreferrer" className="alink">Code</a></button>
               </div>
             </div>
           </li>
           <li className="booking-card b3">
             <div className="book-container">
               <div className="content">
-                <button className="btn"><a href='https://hygieia.sarswat.co/' className="alink">Hygieia</a></button>
-                <button className="btn"><a href='https://github.com/ayush-020198/hygieia-backend' className="alink">Code</a></button>
+                <button className="btn"><a href='https://hygieia.sarswat.co/' target="_blank" rel="noopener noreferrer" className="alink">Hygieia</a></button>
+                <button className="btn"><a href='https://github.com/ayush-020198/hygieia-backend' target="_blank" rel="noopener noreferrer" className="alink">Code</a></button>
               </div>
             </div>
           </li>
           <li className="booking-card b4">
             <div className="book-container">
               <div className="content">
-                <button className="btn"><a href='https://ayush-020198.github.io/dice-game/' className="alink">Dice-Game</a></button>
-                <button className="btn"><a href='https://github.com/ayush-020198/dice-game' className="alink">Code</a></button>
+                <button className="btn"><a href='https://ayush-020198.github.io/dice-game/' target="_blank" rel="noopener noreferrer" className="alink">Dice-Game</a></button>
+                <button className="btn"><a href='https://github.com/ayush-020198/dice-game' target="_blank" rel="noopener noreferrer" className="alink">Code</a></button>
               </div>
             </div>
           </li>
           <li className="booking-card b5">
             <div className="book-container">
               <div className="content">
-                <button className="btn"><a href='https://ayush-020198.github.io/Robot5o/' className="alink">Robo5o</a></button>
-                <button className="btn"><a href='https://github.com/ayush-020198/Robot5o' className="alink">Code</a></button>
+                <button className="btn"><a href='https://ayush-020198.github.io/Robot5o/' target="_blank" rel="noopener noreferrer" className="alink">Robo5o</a></button>
+                <button className="btn"><a href='https://github.com/ayush-020198/Robot5o' target="_blank" rel="noopener noreferrer" className="alink">Code</a></button>
               </div>
             </div>
           </li>
           <li className="booking-card b6">
             <div className="book-container">
               <div className="content">
-                <button className="btn"><a href='https://ayush-020198.github.io/DrumKit/' className="alink">Drum-Kit</a></button>
-                <button className="btn"><a href='https://github.com/ayush-020198/DrumKit' className="alink">Code</a></button>
+                <button className="btn"><a href='https://ayush-020198.github.io/DrumKit/' target="_blank" rel="noopener noreferrer" className="alink">Drum-Kit</a></button>
+                <button className="btn"><a href='https://github.com/ayush-020198/DrumKit' target="_blank" rel="noopener noreferrer" className="alink">Code</a></button>
               </div>
             </div>
           </li>
